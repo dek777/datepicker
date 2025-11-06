cccd1b5a0d7bc6596355f3ccbc380a0fea929683
dek777 <dek191991@yandex.ru>
2025-11-07 00:48:51 +0300

    feat: Сделал компонент datepicker и форму для теста

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400..600&display=swap');
+
+* {
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script setup>
+  import { ref } from 'vue'
+  import Datepicker from '@/components/Datepicker.vue'
+
+  const selectedDate = ref('')
+
+  const selectedLang = ref('ru')
+</script>
+
+<template>
+  <div class="home__container">
+    <div class="home__lang-btns-wrap">
+      <label class="home__lang-btn">
+        <input type="radio" v-model="selectedLang" value="ru" />
+        Русский
+      </label>
+      <label class="home__lang-btn">
+        <input type="radio" v-model="selectedLang" value="en" />
+        Английский
+      </label>
+    </div>
+
+    <input
+      v-model="selectedDate"
+      placeholder="Введите дату в формате ГГГГ-ММ-ДД"
+      type="text"
+      class="home__input"
+    />
+
+    <Datepicker
+      v-model="selectedDate"
+      :lang="selectedLang"
+    />
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  &__container {
+    width: 100%;
+    max-width: 500px;
+    margin: auto;
+    padding: 32px 16px;
+  }
+
+  &__lang-btns-wrap {
+    margin-bottom: 24px;
+  }
+
+  &__lang-btn {
+    display: block;
+    margin-bottom: 4px;
+  }
+
+  &__input {
+    margin-bottom: 24px;
+    width: 350px;
+    padding: 10px 16px;
+    outline: 0;
+    border: 1px solid $borders-color;
+    border-radius: $controls-border-radius;
+    transition: $transition;
+
+    &:hover {
+      border-color: $primary;
+    }
+  }
+}
+</style>
